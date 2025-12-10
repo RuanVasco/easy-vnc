e1e62711094e5ccb57577669a5c360364c13c379
Ruan Vasconcelos <ruanvasconcelos898@gmail.com>
2025-12-10 14:24:00 -0300

    refactor: restructure project as monorepo and rename client crate

--- crates/easy-client/src/main.rs
+++ crates/easy-client/src/main.rs
@@ -1,17 +1,15 @@
-use gtk4::prelude::*;
 use gtk4::Application;
+use gtk4::prelude::*;
 
+mod config;
 mod model;
 mod service;
 mod ui;
-mod config;
 
-const APP_ID: &str = "com.github.RuanVasco.easy-remote";
+const APP_ID: &str = "com.github.RuanVasco.easy-client";
 
 fn main() {
-    let app = Application::builder()
-        .application_id(APP_ID)
-        .build();
+    let app = Application::builder().application_id(APP_ID).build();
 
     app.connect_activate(ui::build);
     app.run();
